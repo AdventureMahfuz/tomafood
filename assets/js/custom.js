d0e367111c51ae2f549ee674acda3a0c86d35700
AdventureMahfuz <adventure.mahfuz@gmail.com>
2019-07-13 20:53:32 +0600

    design some section on front page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,15 @@
 (function($) {
     $(document).ready(function() {
+        //sticky menu when scroll
+        $(window).on('scroll', function() {
+            var scroll = $(window).scrollTop();
+            if (scroll > 0) {
+                $(".header-section").addClass("sticky_top_section");
+            } else {
+                $(".header-section").removeClass("sticky_top_section");
+            }
+        });
+
         //hero slider
         $('#hero-slider').owlCarousel({
             animateIn: 'fadeIn',
@@ -25,40 +35,10 @@
                 }
             }
         });
-        //slider animation
-        /*$('#slider').on('translate.owl.carousel',function () {
-            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').removeClass('animated fadeInUp').css('opacity','0');
-            //$('.hero-cont-btn a').removeClass('animated slideInLeft').css('opacity','0');
-            //$('.hero-more-link a').removeClass('animated slideInRight').css('opacity','0');
-        });
-        $('#slider').on('translated.owl.carousel',function () {
-            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').addClass('animated fadeInUp').css('opacity','0');
-            //$('.hero-cont-btn a').addClass('animated slideInLeft').css('opacity','1');
-            //$('.hero-more-link a').addClass('animated slideInRight').css('opacity','1');
-        });*/
-
-
-
-
-        //WoW js activation
-        //new WOW().init();
-
-        //responsive slick nav menu activation
-        /*$('#nav').slicknav({
-            prependTo:'.responsive-mobile-menu',
-        });*/
-
-        // magnific popup activation
-        /* $('.popup-img').magnificPopup({
-             gallery: {
-                 enabled: true
-             },
-             type: 'image'
-         });*/
 
         //back to top show when scroll
-        /*$(".back_to_top i.fa").css("display", "none");
-        $(window).on('scroll', function () {
+        $(".back_to_top i.fa").css("display", "none");
+        $(window).on('scroll', function() {
             var scroll = $(window).scrollTop();
             if (scroll < 300) {
                 $(".back_to_top i.fa").css("display", "none");
@@ -66,11 +46,11 @@
                 $(".back_to_top i.fa").css("display", "block");
             }
         });
-        $(".back_to_top i.fa").on("click",function () {
+        $(".back_to_top i.fa").on("click", function() {
             $("html").animate({
                 scrollTop: 0
             }, 800);
-        });*/
+        });
 
     });
 })(jQuery);
